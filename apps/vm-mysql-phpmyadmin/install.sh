93062273ca211494f331a64d4d9b5d37d0100343
bdereims <bdereims@gmail.com>
2016-09-15 07:49:45 +0000

    remove all apt and replace by apt-get

--- apps/vm-mysql-phpmyadmin/install.sh
+++ apps/vm-mysql-phpmyadmin/install.sh
@@ -3,7 +3,7 @@
 #apt -y update
 
 export DEBIAN_FRONTEND=noninteractive
-apt -y install mysql-client mysql-server phpmyadmin
+apt-get -y install mysql-client mysql-server phpmyadmin
 
 sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
 echo "Include /etc/phpmyadmin/apache.conf" >> /etc/apache2/apache2.conf
