7be8825410339b7d8d56bee076cc9db0016c80f7
Brice Dereims <bdereims@gmail.com>
2016-12-13 20:40:26 +0100

    add yaml example

---
+++ Photon-Deploy/install_photon.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#bdereims@vmware.com
+
+
+[ "${1}" == "" ] && echo "usage: ${0} deploy_env" && exit 1
+[ ! -f "${1}" ] && echo "error: file '${1}' does not exist" && exit 1
+
+. ./"${1}"
+
+### Local vars ####
+
+###################
+
+photon target set http://${INSTALLVM}:9000
+photon system deploy photon.yaml
