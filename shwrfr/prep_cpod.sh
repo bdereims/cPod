733c2a05f35d9d1334d82c5f1d233ba4181e16ff
Brice Dereims <bdereims@gmail.com>
2017-09-25 19:41:41 +0000

    -

--- shwrfr/prep_cpod.sh
+++ shwrfr/prep_cpod.sh
@@ -14,8 +14,8 @@ main() {
 	CPOD_NAME_LOWER=$( echo ${CPOD_NAME} | tr '[:upper:]' '[:lower:]' )
 	IP=$( cat ${HOSTS} | grep ${CPOD_NAME_LOWER} | cut -f1 )
 
-	scp compute/prep_and_add_esx.sh root@${CPOD_NAME_LOWER}:.
-	ssh root@${CPOD_NAME_LOWER} "./prep_and_add_esx.sh"
+	scp -o StrictHostKeyChecking=no compute/prep_and_add_esx.sh root@${CPOD_NAME_LOWER}:.
+	ssh -o StrictHostKeyChecking=no root@${CPOD_NAME_LOWER} "./prep_and_add_esx.sh"
 }
 
 main $1
