8be4203d4026b316fb900d75eaf7a7a61fcf3423
Brice Dereims <bdereims@gmail.com>
2017-09-25 19:34:19 +0000

    deploy script without env

--- shwrfr/network/list_pool.sh
+++ shwrfr/network/list_pool.sh
@@ -4,4 +4,4 @@
 . ./env
 
 #curl -s -k -u ${NSX_ADMIN}:${NSX_PASSWD} -X GET -H "Accept: application/json" https://${NSX}/api/2.0/services/ipam/pools/scope/globalroot-0 | jq '. | .["allScopes"] | .[0] | {name: .name, id: .id}'
-curl -s -k -u ${NSX_ADMIN}:${NSX_PASSWD} -X GET -H "Accept: application/json" https://${NSX}/api/2.0/services/ipam/pools/scope/globalroot-0 
+curl -s -k -u ${NSX_ADMIN}:${NSX_PASSWD} -X GET -H "Accept: application/json" https://${NSX}/api/2.0/services/ipam/pools/scope/globalroot-0 | jq
