24d144f41171f1d737b05dbf427dd559d55fa68a
Brice Dereims <bdereims@gmail.com>
2018-04-30 17:38:54 +0000

    some mods

--- shwrfr/zz.sh
+++
@@ -1,7 +0,0 @@
-	STATUS=1 
-	while [ ${STATUS} -gt 0 ]
-        do
-                echo "Waiting for cPodRouter of ${CPOD_NAME_LOWER}"
-		STATUS=$( ping -c 1 172.16.0.99 2>&1 > /dev/null ; echo $? )
-		STATUS=$(expr $STATUS)
-        done
