891584efad78b4cf74d50890d9b77c1a2d782e74
Brice Dereims <bdereims@gmail.com>
2018-06-25 07:09:06 +0000

    add the cluster choice

--- shwrfr/extra/check_space.sh
+++ shwrfr/extra/check_space.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 . ./govc_env
+. ./env
 
-DELLVSAN=$( govc datastore.info DELL-VSAN | grep Free | sed -e "s/^.*://" -e "s/GB//" -e "s/ //g" )
+CLUSTER=$( echo $CLUSTER | tr '[:lower:]' '[:upper:]' )
+
+DELLVSAN=$( govc datastore.info ${CLUSTER}-VSAN | grep Free | sed -e "s/^.*://" -e "s/GB//" -e "s/ //g" )
 DELLVSAN=$( echo "${DELLVSAN}/1" | bc )
 DELLVSAN=$( expr ${DELLVSAN} )
 
-if [ ${DELLVSAN} -lt 4500 ]; then
+if [ ${DELLVSAN} -lt 7000 ]; then
 	echo "No more space!"
 	exit 1
 fi
