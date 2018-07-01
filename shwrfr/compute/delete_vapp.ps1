4c8a8ff9cfd726915e875830a8a80e6b244582e5
Brice Dereims <bdereims@gmail.com>
2018-07-01 20:30:13 +0000

    improve cPod deletion

--- shwrfr/compute/delete_vapp.ps1
+++ shwrfr/compute/delete_vapp.ps1
@@ -11,7 +11,7 @@ $cPodName = "###CPOD_NAME###"
 Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false -DefaultVIServerMode multiple
 Connect-VIServer -Server $Vc -User $vcUser -Password $vcPass
 
-$VApp = Get-VApp -Name cPod-$cPodName -Location ( Get-Cluster -Name $Cluster ) 
+$VApp = Get-VApp -Name cPod-$cPodName -Location ( Get-Datacenter -Name $Datacenter ) 
 Stop-VApp -Force -VApp $VApp -Confirm:$false 
 Remove-VApp -VApp $VApp -Confirm:$false -DeletePermanently:$true
 
